b149d1a3d4de3f4f5685e4b9a45cb19cb5f7cf80
iustitia <jkubisztal@gmail.com>
2019-02-24 11:34:00 +0100

    model representation

--- pokedex/models.py
+++ pokedex/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class Pokemon(models.Model):
     name = models.CharField(max_length=200)
     is_yellow = models.BooleanField()
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.name
