060050cec2c7297f572c9088aacee989c541d6aa
iustitia <jkubisztal@gmail.com>
2020-10-21 21:28:57 +0200

    Api with viewsets

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from .models import Pokemon
 from .views import PokemonListView, PokemonDetailView, add_new_manually, AddCreateView, check_date
-from .viewsets import PokemonListAPIView
+from .viewsets import PokemonListAPIView, PokemonListAPIViewSet
 
 urlpatterns = [
     path('', PokemonListView.as_view()),
@@ -12,5 +13,10 @@ urlpatterns = [
     path('add_manually', add_new_manually),
     path('add_detail_view', AddCreateView.as_view(), name='add-create-view'),
     path('check_if_older/<int:pokemon_id>/', check_date),
-    path('api/pokemons/', PokemonListAPIView.as_view(queryset=Pokemon.objects.all()), name="pokemons"),
+    path('api/v1/pokemon/', PokemonListAPIView.as_view(queryset=Pokemon.objects.all()), name="pokemons"),
 ]
+
+router = DefaultRouter()
+router.register(r'api/v2/pokemon', PokemonListAPIViewSet)
+
+urlpatterns += router.urls
--- pokedex/viewsets.py
+++ pokedex/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
 
 from pokedex.models import Pokemon
 from pokedex.serializers import PokemonSerializer
@@ -8,3 +9,8 @@ class PokemonListAPIView(ListAPIView):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
     http_method_names = ['get']
+
+
+class PokemonListAPIViewSet(viewsets.ModelViewSet):
+    serializer_class = PokemonSerializer
+    queryset = Pokemon.objects.all()
